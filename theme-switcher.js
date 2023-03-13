898c4035b4f253c48c25733bc0dabdff9ec87fc3
Thu Le <hello@thu-le.com>
2023-03-13 21:36:25 +0700

    Introduce theme switcher

---
+++ theme-switcher.js
@@ -0,0 +1,23 @@
+const switcher = document.getElementById('theme-switcher');
+const body = document.body;
+
+// Check if there is a saved theme preference in local storage
+const theme = localStorage.getItem('theme');
+if (theme) {
+  body.classList.add(theme);
+  if (theme === 'dark') {
+    switcher.innerHTML = '&#x263C;';
+  }
+}
+
+switcher.addEventListener('click', () => {
+  // Toggle the 'dark' class on the body element
+  body.classList.toggle('dark');
+
+  // Save the theme preference to local storage
+  const theme = body.classList.contains('dark') ? 'dark' : '';
+  localStorage.setItem('theme', theme);
+
+  // Update the button text
+  switcher.innerHTML = body.classList.contains('dark') ? '&#x263C;' : '&#x263E;';
+});
