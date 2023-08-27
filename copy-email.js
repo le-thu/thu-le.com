52048c6a9f9dba6d3e1e01ced4212fb2ae112ccc
Thu Le <hello@thu-le.com>
2023-08-27 15:14:08 +0700

    Update copy-email.js

--- copy-email.js
+++ copy-email.js
@@ -3,18 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const emailAnchor = tooltipContainer.querySelector('.email');
     const tooltip = tooltipContainer.querySelector('.tooltip');
 
-    let isTooltipVisible = false;
-
-    const showTooltip = () => {
-        tooltip.style.visibility = 'visible';
-        isTooltipVisible = true;
-    };
-
-    const hideTooltip = () => {
-        tooltip.style.visibility = 'hidden';
-        isTooltipVisible = false;
-    };
-
     tooltipContainer.addEventListener('click', () => {
         const email = emailAnchor.textContent.trim();
         navigator.clipboard.writeText(email).then(() => {
@@ -25,32 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    tooltipContainer.addEventListener('mouseenter', () => {
-        showTooltip();
-    });
-
-    tooltipContainer.addEventListener('mouseleave', () => {
-        hideTooltip();
-    });
-
     tooltipContainer.addEventListener('touchstart', (event) => {
         event.preventDefault(); // Prevents the click event from firing immediately
-        if (!isTooltipVisible) {
-            showTooltip();
-        }
+        tooltip.style.visibility = 'visible';
     });
 
     tooltipContainer.addEventListener('touchend', () => {
-        if (isTooltipVisible) {
-            const email = emailAnchor.textContent.trim();
-            navigator.clipboard.writeText(email).then(() => {
-                tooltip.textContent = 'Copied';
-                setTimeout(() => {
-                    tooltip.textContent = 'Copy my email address';
-                }, 1200);
-            });
-
-            hideTooltip();
-        }
+        const email = emailAnchor.textContent.trim();
+        navigator.clipboard.writeText(email).then(() => {
+            tooltip.textContent = 'Copied';
+            setTimeout(() => {
+                tooltip.textContent = 'Copy my email address';
+            }, 1200);
+        });
     });
 });
