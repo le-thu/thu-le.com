b0d05f8b249441a8c6df8c1b082acb3fb1e65324
Thu Le <thu@thu-le.com>
2024-07-20 12:53:29 +0700

    Interaction improvement
    Copy email icon change after click

--- js/copy-email.js
+++ js/copy-email.js
@@ -1,22 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const tooltipContainer = document.querySelector('.tooltip-container');
     const emailAnchor = tooltipContainer.querySelector('.email');
+    const emailText = emailAnchor.querySelector('.email-text');
     const tooltip = tooltipContainer.querySelector('.tooltip');
+    const icon = emailAnchor.querySelector('ion-icon');
 
     const handleInteraction = (event) => {
         if (event.type === 'touchstart') {
-            event.preventDefault(); // Prevents the click event from firing immediately
+            event.preventDefault();
             tooltip.style.visibility = 'visible';
         } else {
             const email = emailAnchor.textContent.trim();
             navigator.clipboard.writeText(email).then(() => {
                 tooltip.textContent = 'Copied';
+                icon.setAttribute('name', 'checkmark-circle');
                 setTimeout(() => {
                     tooltip.textContent = 'Copy thu@thu-le.com';
-                }, 1200);
+                    icon.setAttribute('name', 'copy-outline');
+                }, 1000);
+
+                if (window.clicky) {
+                    clicky.event('Email Copy', 'User copied email address', email);
+                }
             });
         }
     };
+
+    const adjustTooltipPosition = () => {
+        const emailTextWidth = emailText.offsetWidth;
+        tooltip.style.left = `calc(50% - ${emailTextWidth / 2}px)`;
+    };
+
+    adjustTooltipPosition();
 
     tooltipContainer.addEventListener('click', handleInteraction, {
         passive: true,
@@ -27,4 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
     tooltipContainer.addEventListener('touchend', handleInteraction, {
         passive: true,
     });
+
+    window.addEventListener('resize', adjustTooltipPosition);
 });
