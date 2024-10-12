af3ac025cf8580bd27edb6eeac5446fa6e16e097
Thu Le <thu@thu-le.com>
2024-10-12 16:19:31 +0700

    Cleanup

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const carousel = document.querySelector('.carousel');
 
     if (carousel) {
